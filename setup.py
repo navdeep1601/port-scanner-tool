f96c393e181710b8b1b2ddb95f5f2945ba99d3d8
navdeep1601 <navdeepgupta792@gmail.com>
2026-04-10 10:15:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='port-scanner-tool',
+    name='port_scanner_tool',
     version='1.0.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
